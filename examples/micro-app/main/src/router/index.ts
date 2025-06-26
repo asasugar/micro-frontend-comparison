7f59ae1dc15c1a39c45f5dd890b1736376fcd333
Jay Hsueh <xiongjie.xue@lkcoffee.com>
2025-06-26 16:28:08 +0800

    perf: ⚡️ micro-app 子应用路由取配置

--- examples/micro-app/main/src/router/index.ts
+++ examples/micro-app/main/src/router/index.ts
@@ -1,6 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
 import { h } from 'vue';
+import { getMainAppConfig } from '../../../../../configs/shared/app.config';
+import type { SubAppConfig } from '../../../../../configs/shared/types';
+import Home from '../views/Home.vue';
+
+const mainConfig = getMainAppConfig('micro-app');
+
+// 生成子应用路由配置
+const generateSubAppRoutes = () => {
+  return mainConfig.subApps.map((app: SubAppConfig) => ({
+    path: app.activeRule,
+    name: app.name,
+    component: {
+      render: () =>
+        h('micro-app', {
+          name: app.name,
+          url: app.entry,
+        }),
+    },
+  }));
+};
 
 const router = createRouter({
   history: createWebHistory(),
@@ -10,22 +29,7 @@ const router = createRouter({
       name: 'home',
       component: Home,
     },
-    {
-      path: '/vue-app',
-      name: 'vue',
-      component: {
-        render: () => h('micro-app', { name: 'vue-app', url: 'http://localhost:8002/' }),
-      },
-      // component: () => import('../views/vue-app.vue'),
-    },
-    {
-      path: '/react-app',
-      name: 'react',
-      component: {
-        render: () => h('micro-app', { name: 'react-app', url: 'http://localhost:8003/' }),
-      },
-      // component: () => import('../views/react-app.vue'),
-    },
+    ...generateSubAppRoutes(),
   ],
 });
 
